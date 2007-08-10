e881000941ea96b6b6f0010f929c5c5c789522f0
Daniel Stenberg <daniel@haxx.se>
2007-08-10 10:40:21 +0000

    initial add from my private repo, 'roffit' was internal revision 1.17

---
+++ release.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+my @files=('CHANGES', 'roffit', 'roffit.1');
+
+my $version=$ARGV[0];
+
+if(!$version) {
+    print "Usage: release.pl <version>\n";
+    exit;
+}
+
+my $dir = "roffit-$version";
+
+mkdir $dir;
+
+for(@files) {
+    `cp $_ $dir`;
+}
+
+`tar czf roffit-${version}.tar.gz $dir`;
+
+`rm -rf $dir`;
