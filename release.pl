24cc5f8bd8a51044c25b1f9bf489e90b0c699d51
Daniel Stenberg <daniel@haxx.se>
2023-06-21 09:50:27 +0200

    GHA: add a REUSE license and copyright CI job
    Closes #34

--- release.pl
+++ release.pl
@@ -1,4 +1,21 @@
 #!/usr/bin/perl
+##########################################################################
+#
+# Copyright (C) Daniel Stenberg, <daniel@haxx.se>, et al.
+#
+# This software is licensed as described in the file LICENSE, which
+# you should have received as part of this distribution.
+#
+# You may opt to use, copy, modify, merge, publish, distribute and/or sell
+# copies of the Software, and permit persons to whom the Software is
+# furnished to do so, under the terms of the COPYING file.
+#
+# This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+# KIND, either express or implied.
+#
+# SPDX-License-Identifier: curl
+#
+##########################################################################
 
 my @files=('CHANGES', 'roffit', 'roffit.1', 'LICENSE', 'Makefile', 'README');
 
